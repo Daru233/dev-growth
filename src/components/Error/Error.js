a70e0d1258e4b06132c986a5577ff78366586c4b
Michael Malto <michaeldm233@gmail.com>
2022-05-06 14:57:38 +0100

    added image for not found page

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import not_found from './not_found.png'
 
 export const Error = () => {
   return (
     <div className='page_container'>
-      <p>Error, Page not Found</p>
+      <img src={not_found} alt={'Not Found'} style={{marginLeft: '5%' ,height: '70%', width: '70%'}}/>
     </div>
   )
 }
